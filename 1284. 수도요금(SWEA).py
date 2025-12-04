a7ef7a248d3080833c0399575a7edab963bb016a
5000billions <cpop02@naver.com>
2025-12-05 00:12:36 +0900

    Add files via upload

--- 1284. 수도요금(SWEA).py
+++ 1284. 수도요금(SWEA).py
@@ -3,12 +3,21 @@ for tc in range( 1 , T + 1 ):
     '''
     P = A사 리터당 돈
     Q = B사 기본 요금
-    R = 리터 이하일시 Q 요금
-    S = 리터 초과시 Q 요금
+    R = R리터 이하일시 Q 기본요금
+    S = R리터 초과시 리터당 추가 요금
     W = 총 사용량
     '''
     
     P, Q, R, S, W = map(int, input().split())
-print(P)
 
-      
+    A_use = P * W
+    B_use = 0
+
+    if R < W:
+        B_use = Q + (W - R) * S
+    else: 
+        B_use = Q
+
+    Cheap_Company = min(A_use, B_use)
+
+    print(f'#{tc} {Cheap_Company}')
